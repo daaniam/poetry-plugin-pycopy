a5aa9ab34893f132531dc1e284330789d40be9f7
Daniel Fajt <daniel29se@gmail.com>
2022-10-18 21:48:11 +0200

    update

--- src/plugin.py
+++ src/plugin.py
@@ -1,9 +1,18 @@
+from traceback import print_tb
 from cleo.events.console_events import TERMINATE
 from cleo.events.console_command_event import ConsoleCommandEvent
 from cleo.events.event_dispatcher import EventDispatcher
 from poetry.console.application import Application
 from poetry.console.commands.env_command import EnvCommand
 from poetry.plugins.application_plugin import ApplicationPlugin
+from pathlib import Path
+from poetry.core.pyproject.toml import PyProjectTOML
+from poetry.console.commands.version import VersionCommand
+from io import StringIO
+
+
+def create_line(k, v):
+    return f'__{k} = "{v}"\n'
 
 
 class PushToPyprojectPy(ApplicationPlugin):
@@ -11,20 +20,42 @@ class PushToPyprojectPy(ApplicationPlugin):
         application.event_dispatcher.add_listener(TERMINATE, self.push)
 
     def push(
-        self, event: ConsoleCommandEvent, event_name: str, dispatcher: EventDispatcher
+        self,
+        event: ConsoleCommandEvent,
+        event_name: str,
+        dispatcher: EventDispatcher,
     ):
-        print("Push META called")
+        project_root = Path(__name__).parent.absolute()
+        toml_file = Path.joinpath(project_root, "pyproject.toml")
+        if not toml_file.exists():
+            print("No toml file")
 
-    # def load_dotenv(
-    #     self, event: ConsoleCommandEvent, event_name: str, dispatcher: EventDispatcher
-    # ) -> None:
-    #     command = event.command
-    #     if not isinstance(command, EnvCommand):
-    #         return
+        toml_data = PyProjectTOML(path=toml_file).file.read()
+        # poetry_tool = toml_data["tool"]["Poetry"]
 
-    #     io = event.io
+        tool_poetry = toml_data["tool"]["poetry"]
+        tool_poetry_name = toml_data["tool"]["poetry"]["name"]
 
-    #     if io.is_debug():
-    #         io.write_line("<debug>Loading environment variables.</debug>")
+        tool_poetry_bump = toml_data["tool"]["poetry-bump"]
+        bump_keys = list(toml_data["tool"]["poetry-bump"]["keys"])
+        bump_dest_dir = toml_data["tool"]["poetry-bump"]["dest_dir"]
+        bump_dest_file = toml_data["tool"]["poetry-bump"]["dest_file"]
 
-    #     load_dotenv()
+        print("bump_keys", bump_keys)
+        print("bump_dest_dir", bump_dest_dir)
+        print("bump_dest_file", bump_dest_file)
+
+        pyproject_py = Path.joinpath(project_root, bump_dest_dir).joinpath(
+            bump_dest_file
+        )
+        print("pyproject_py", pyproject_py)
+
+        parsed_data = {k: tool_poetry[k] for k in bump_keys if k in tool_poetry}
+
+        print("parsed_data", parsed_data)
+
+        lines = [create_line(k, v) for k, v in parsed_data.items()]
+        print("lines", lines)
+
+        with open(pyproject_py, "w+", encoding="utf-8") as f:
+            f.writelines(lines)
